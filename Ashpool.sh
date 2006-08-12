b76be81d54c6b51c4eb203a4511db1514a10eb68
robrohan <robrohan@1a912658-a619-0410-aee8-37565469daf4>
2006-08-12 19:33:48 +0000

    updated the build script and launch script to work properly
    git-svn-id: https://svn9.cvsdude.com/robrohan/Ashpool/trunk@8 1a912658-a619-0410-aee8-37565469daf4

--- Ashpool.sh
+++ Ashpool.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-ASHPOOL_CLASSPATH=bsf.jar:saxon8.jar:xercesImpl.jar:xml-apis.jar:jruby.jar:Ashpool.jar
+LIB_DIR=lib
+ASHPOOL_CLASSPATH=Ashpool.jar:$LIB_DIR/bsf.jar:$LIB_DIR/saxon8.jar:$LIB_DIR/xercesImpl.jar:$LIB_DIR/xml-apis.jar:$LIB_DIR/jruby.jar:$LIB_DIR/js.jar
 ASHPOOL_SAXDRIVER=org.apache.xerces.parsers.SAXParser
 
 # Used to run the built in command line application
