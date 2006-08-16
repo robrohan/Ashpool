ab0b2a0b5f298488b71f5375801d3310957ab199
robrohan <robrohan@1a912658-a619-0410-aee8-37565469daf4>
2006-08-16 17:27:26 +0000

    added a larger heap to the shell script and added jdbc settings to the readme
    git-svn-id: https://svn9.cvsdude.com/robrohan/Ashpool/trunk@11 1a912658-a619-0410-aee8-37565469daf4

--- Ashpool.sh
+++ Ashpool.sh
@@ -5,7 +5,7 @@ ASHPOOL_CLASSPATH=Ashpool.jar:$LIB_DIR/bsf.jar:$LIB_DIR/saxon8.jar:$LIB_DIR/xerc
 ASHPOOL_SAXDRIVER=org.apache.xerces.parsers.SAXParser
 
 # Used to run the built in command line application
-java -Dorg.xml.sax.driver=$ASHPOOL_SAXDRIVER -cp $ASHPOOL_CLASSPATH com.rohanclan.ashpool.Ashpool $@
+java -Xmx256M -Dorg.xml.sax.driver=$ASHPOOL_SAXDRIVER -cp $ASHPOOL_CLASSPATH com.rohanclan.ashpool.Ashpool $@
 
 # Used to run the JDBC test application
 #java -Dorg.xml.sax.driver=$ASHPOOL_SAXDRIVER -cp $ASHPOOL_CLASSPATH com.rohanclan.ashpool.JdbcTest $@
--- readme.txt
+++ readme.txt
@@ -3,3 +3,8 @@ Compiled with:
 bsf.jar 2.3.0 http://jakarta.apache.org/site/downloads/downloads_bsf.html
 saxon8.jar 8-7j http://saxon.sf.net
 Xerces-J 2.8.0 http://www.apache.org/dist/xml/xerces-j/
+
+Typical Needed Info:
+
+Driver: com.rohanclan.ashpool.jdbc.Driver
+URL: jdbc:ashpool:file://[path]
